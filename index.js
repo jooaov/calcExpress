743d7a43b26b35508a13dcb0c9bae10c4126f85a
jooaov <jjoao.jv13@gmail.com>
2019-08-20 11:37:00 -0300

    finalizado layout 1.0 e add jquery

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express=require("express");
 const app=express();
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-
+app.use('/js', express.static(__dirname + '/static/js'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+console.log(__dirname)
 
 app.use(express.static('static'));
 app.listen(3000, function () {
